94ef820a221ec68e7a10292ea8cb4c5105af26d7
zoemurphy <zoemurphy@localhost>
2023-10-12 16:52:37 -0500

    Trying Again

--- CamelotJava/src/com/entities/ActionSequence.java
+++ CamelotJava/src/com/entities/ActionSequence.java
@@ -1,5 +1,16 @@
 package com.entities;
 
 public class ActionSequence implements IAction{
-
+	private ArrayList<IAction> actions = new ArrayList<>();
+	
+	public ActionSequence() {
+		return;
+	}
+	public void add(IAction, action) {
+		return;
+	}
+	public void execute() {
+		return;
+	}
+	
 }
