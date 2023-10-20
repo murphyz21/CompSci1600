2850bb31d3df13b49f9e76e764cb3055e4397c30
ksull <ksull@10.129.111.202>
2023-10-20 12:32:51 -0500

    pushit l

--- CamelotJava/src/ShortStory.java
+++ CamelotJava/src/ShortStory.java
@@ -1,6 +1,9 @@
 import com.entities.Things.ThingNames;
 
 public class ShortStory implements IStory{
+	private Character edith, king, guard1, guard2, guard3, alchemist, campBegger1, campBegger2, campBegger3;
+	private Place startCyard,camp, cYard1, cYard2, cYrad3, cYard4, alchemyShop,cYard5, cYard6, cYard7, cYard8;
+	private Item 
 	
 	public INode getRoot() {
 		return new Node("root");
@@ -8,55 +11,48 @@ public class ShortStory implements IStory{
 	
 	public void getThings() {
 		//Start
-		var edith = new Character(ThingNames.Edith);
-		var startCyard = new Place(ThingNames.startCyard, Places.Courtyard);
-		var greenbook = new Item(ThingNames.greenbook, Items.GreenBook);
+		edith = new Character(ThingNames.Edith);
+		startCyard = new Place(ThingNames.startCyard, Places.Courtyard);
+		greenbook = new Item(ThingNames.greenbook, Items.GreenBook);
+		king = new Character(ThingNames.king);
 		
 		//Left Side
-		var camp = new Place(ThingNames.camp, Places.Camp);
-		var spellBook = new Item(ThingNames.spellBook, Items.SpellBook);
-		var cYard1 = new Place(ThingNames.Courtyard, Places.Courtyard);
-		var friendKing = new Character(ThingNames.friendKing);
-		var badTalkKing = new Character(ThingNames.badTalkKing);
+		camp = new Place(ThingNames.camp, Places.Camp);
+		spellBook = new Item(ThingNames.spellBook, Items.SpellBook);
+		cYard1 = new Place(ThingNames.Courtyard, Places.Courtyard);
+		campBegger1 = new Character(ThingNames.campBegger1);
+		campbegger2 = new Character(ThingNames.campBegger2);
+		campBegger3 = new Character(ThingNames.campBegger3);
 		
-		var cYard2 = new Place(ThingNames.cYard2, Places.Courtyard);
-		var hurtKing = new Character(ThingNames.hurtKing);
-		var goodSpellKing = new Character(ThingNames.goodSpellKing);
+		cYard2 = new Place(ThingNames.cYard2, Places.Courtyard);
 		
-		var sword = new Item(ThingNames.sword, Items.Sword);
-		var helmet = new Item(ThingNames.helmet, Items.Helmet);
-		var torch = new Item(ThingNames.torch, Items.LitTorch);
+		sword = new Item(ThingNames.sword, Items.Sword);
+		helmet = new Item(ThingNames.helmet, Items.Helmet);
+		torch = new Item(ThingNames.torch, Items.LitTorch);
 		
-		var cYard3 = new Place(ThingNames.cYard3, Places.Courtyard);
-		var youDieKing = new Character(ThingNames.youdieKing);
-		var swordBecomesKing = new Character(ThingNames.swordBecomesKing);
+		cYard3 = new Place(ThingNames.cYard3, Places.Courtyard);
+		youDieKing = new Character(ThingNames.youdieKing);
 		
-		var cYard4 = new Place(ThingNames.cYard4, Places.Courtyard);
-		var torchBecomesKing = new Character(ThingNames.torchBecomesKing);
-		var guardArrestYou = new Character(ThingNames.guardArrestYou);
+		
+		cYard4 = new Place(ThingNames.cYard4, Places.Courtyard);
+		guard1 = new Character(ThingNames.guard1);
 		
 		//Right Side
-		var evilbook = new Item(ThingNames.evilbook, Items.EvilBook);
-		var alchemyShop = new Place(Thingnames.alchemyShop, Places.AlchemyShop);
-		var poison = new Item(ThingNames.poison, Items.GreenPotion);
+		evilbook = new Item(ThingNames.evilbook, Items.EvilBook);
+		alchemyShop = new Place(Thingnames.alchemyShop, Places.AlchemyShop);
+		alchemist - new Character(ThingNames.alchemist);
+		poison = new Item(ThingNames.poison, Items.GreenPotion);
 		
-		var cYard5 = new Place(ThingNames.cYard5, Places.Courtyard);
-		var drinksPoisonKing = new Character(ThingNames.drinksPoisonKing);
-		var killsyouKing = new Character(ThingNames.killsyouKing);
+		cYard5 = new Place(ThingNames.cYard5, Places.Courtyard);
 		
-		var cYard6 = new Place(ThingNames.cYard6, Places.Courtyard);
-		var weakPoisonKing = new Character(ThingNames.weakPoisonKing);
-		var guard2 = new Character(ThingNames.guard2);
+		cYard6 = new Place(ThingNames.cYard6, Places.Courtyard);
+		guard2 = new Character(ThingNames.guard2);
 		
-		var bluePotion = new Item(ThingNames.bluePotion, Items.BluePotion);
-		var greenPotion = new Item(ThingNames.greenPotion, Items.GreenPotion);
-		var cYard6 = new Place(ThingNames.cYard6, Places.Courtyard);
-		var noPowersArrestKing = new Character(Thingnames.noPowerArrestKing);
-		var noPowersKillsKing = new Character(ThingNames.noPowersKillsKing);
+		bluePotion = new Item(ThingNames.bluePotion, Items.BluePotion);
+		greenPotion = new Item(ThingNames.greenPotion, Items.GreenPotion);
+		cYard7 = new Place(ThingNames.cYard7, Places.Courtyard);
 		
-		var cYard7 = new Place(ThingNames.cYard7, Places.Courtyard);
-		var powersArrestKing = new Character(ThingNames.powersArrestKing);
-		var guard3 = new Character(ThingNames.guard3);
-		var powersBecomeKing = new Character(ThingNames.powersBecomeKing);	
+		cYard8 = new Place(ThingNames.cYard8, Places.Courtyard);
+		guard3 = new Character(ThingNames.guard3);
 	}
 }
