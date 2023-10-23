f0ef2503a5ba979583b1ad76603dcdc7c9f457f4
ksull <ksull@10.129.111.202>
2023-10-22 22:49:40 -0500

    i tried the actions

--- CamelotJava/src/com/actions/Drink.java
+++ CamelotJava/src/com/actions/Drink.java
@@ -1,5 +1,22 @@
 package com.actions;
 
-public class Drink {
-
+public abstract class Drink implements IAction{
+	private Character drink;
+	
+	public Drink(Character drink) {
+		this.drink = drink;
+	}
+	
+	public String getName() {
+		return "drink";
+	}
+	
+	public boolean getShouldWait() {
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s(%s)", getName(), drink.getName(drink));
+	}
 }
--- CamelotJava/src/com/actions/SetDialog.java
+++ CamelotJava/src/com/actions/SetDialog.java
@@ -1,5 +1,21 @@
 package com.actions;
 
-public class SetDialog {
-
+public class SetDialog implements IAction{
+	private String line;
+	
+	public SetDialog(String line) {
+		this.line = line;
+	}
+	
+	public String getName() {
+		return "SetDialog";
+	}
+	
+	public boolean getShouldWait() {
+		return true;
+	}
+	
+	public String toString() {
+		return String.format("%s(%s)", getName(), line.getName());
+	}
 }
