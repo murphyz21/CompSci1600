8ad0d52d86d12a3ae7ba5930df3bd407f85e1f9b
zoemurphy <zoemurphy@localhost>
2023-11-28 12:03:48 -0600

    try again

--- CamelotJava/src/ShortStory.java
+++ CamelotJava/src/ShortStory.java
@@ -411,8 +411,7 @@ public class ShortStory implements IStory, IAction, IThing, IEntity{
 		sequence.add(new Position(evilbook, Courtyard, "BigStall.Left"));
 		sequence.add(new HideDialog());
 		// comment 
-		// new stuff
-		// commetn2
+		
 		return sequence;
 	}
 	
